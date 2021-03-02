ed9fabbbd72b28b089f5ad38b5598837be80cc9f
Matthew Wire <mjw@mjwconsult.co.uk>
2021-03-02 12:56:20 +0000

    Add button to download csv of tablenames that will be emptied

--- CRM/Mjwcividbtools/Form/ClearData.php
+++ CRM/Mjwcividbtools/Form/ClearData.php
@@ -7,6 +7,8 @@ use CRM_Mjwcividbtools_ExtensionUtil as E;
  */
 class CRM_Mjwcividbtools_Form_ClearData extends CRM_Core_Form {
 
+  public $submitOnce = TRUE;
+
   /**
    * @var array $tables
    */
@@ -35,13 +37,19 @@ class CRM_Mjwcividbtools_Form_ClearData extends CRM_Core_Form {
       $this->add('checkbox', "tabletotruncate_{$tableName}", $tableName);
     }
 
-    $this->addButtons(array(
-      array(
+    $this->addButtons([
+      [
         'type' => 'submit',
-        'name' => E::ts('Submit'),
+        'name' => E::ts('Clear data from tables'),
         'isDefault' => TRUE,
-      ),
-    ));
+        'subName' => 'cleardata',
+      ],
+      [
+        'type' => 'submit',
+        'name' => E::ts('Get list of tables to clear'),
+        'subName' => 'listtablestoclear',
+      ],
+    ]);
 
     // export form elements
     $this->assign('elementNames', $this->getRenderableElementNames());
@@ -61,41 +69,62 @@ class CRM_Mjwcividbtools_Form_ClearData extends CRM_Core_Form {
     // Validation
     CRM_Utils_Type::validate($values['contact_ids'], 'CommaSeparatedIntegers', TRUE);
 
-    // Action
-    $logging = new CRM_Logging_Schema();
-    $loggingEnabled = $logging->isEnabled();
-    if ($loggingEnabled) {
-      $logging->disableLogging();
-      $logging->dropAllLogTables();
-    }
-
-    $queries[] = "SET FOREIGN_KEY_CHECKS = 0";
-    foreach ($values as $tableName => $tableValue) {
-      if (($tableName === 'contact_ids') && ($values['tabletotruncate_civicrm_contact'] == 1)) {
-        $queries[] = "DELETE FROM civicrm_contact WHERE id NOT IN (" . $values['contact_ids'] . ")";
-      }
-      if ((substr($tableName, 0, 16) === 'tabletotruncate_') && ($tableValue == 1)) {
-        $table = substr($tableName, 16);
-        if ($table !== 'civicrm_contact') {
-          $queries[] = "DELETE FROM {$table}";
-          $queries[] = "ALTER TABLE {$table} AUTO_INCREMENT=1";
+    $buttonName = $this->controller->getButtonName();
+    if ($buttonName === $this->getButtonName('submit', 'listtablestoclear')) {
+      foreach ($values as $tableName => $tableValue) {
+        if ((substr($tableName, 0, 16) === 'tabletotruncate_') && ($tableValue == 1)) {
+          $tables[] = substr($tableName, 16);
         }
       }
+      $tablesString = implode(',', $tables);
+      header("Content-Description: File Transfer");
+      header("Content-Type: application/octet-stream");
+      header("Content-Transfer-Encoding: text");
+      header("Content-Disposition: attachment; filename=tables.csv");
+      header('Content-Length: '.  strlen($tablesString));
+      header('Expires: 0');
+      header('Cache-Control: must-revalidate');
+      header('Pragma: public');
+      echo $tablesString;
+      exit();
     }
-    foreach ($this->tables['tablesToDrop'] as $dropTableName) {
-      $queries[] = "DROP TABLE {$dropTableName}";
-    }
-    $queries[] = "SET FOREIGN_KEY_CHECKS = 1";
-    foreach ($queries as $query) {
-      CRM_Core_DAO::executeQuery($query);
-    }
-    $maxContact = (int) CRM_Core_DAO::singleValueQuery('select MAX(id) from civicrm_contact');
-    CRM_Core_DAO::executeQuery('ALTER TABLE civicrm_contact AUTO_INCREMENT=' . ($maxContact + 1));
+    elseif ($buttonName === $this->getButtonName('submit', 'cleardata')) {
+      // Action
+      $logging = new CRM_Logging_Schema();
+      $loggingEnabled = $logging->isEnabled();
+      if ($loggingEnabled) {
+        $logging->disableLogging();
+        $logging->dropAllLogTables();
+      }
 
-    CRM_Utils_Cache::singleton()->clear();
-    if ($loggingEnabled) {
-      $logging->enableLogging();
-      civicrm_api3('System', 'createmissinglogtables', []);
+      $queries[] = "SET FOREIGN_KEY_CHECKS = 0";
+      foreach ($values as $tableName => $tableValue) {
+        if (($tableName === 'contact_ids') && ($values['tabletotruncate_civicrm_contact'] == 1)) {
+          $queries[] = "DELETE FROM civicrm_contact WHERE id NOT IN (" . $values['contact_ids'] . ")";
+        }
+        if ((substr($tableName, 0, 16) === 'tabletotruncate_') && ($tableValue == 1)) {
+          $table = substr($tableName, 16);
+          if ($table !== 'civicrm_contact') {
+            $queries[] = "DELETE FROM {$table}";
+            $queries[] = "ALTER TABLE {$table} AUTO_INCREMENT=1";
+          }
+        }
+      }
+      foreach ($this->tables['tablesToDrop'] as $dropTableName) {
+        $queries[] = "DROP TABLE {$dropTableName}";
+      }
+      $queries[] = "SET FOREIGN_KEY_CHECKS = 1";
+      foreach ($queries as $query) {
+        CRM_Core_DAO::executeQuery($query);
+      }
+      $maxContact = (int) CRM_Core_DAO::singleValueQuery('select MAX(id) from civicrm_contact');
+      CRM_Core_DAO::executeQuery('ALTER TABLE civicrm_contact AUTO_INCREMENT=' . ($maxContact + 1));
+
+      CRM_Utils_Cache::singleton()->clear();
+      if ($loggingEnabled) {
+        $logging->enableLogging();
+        civicrm_api3('System', 'createmissinglogtables', []);
+      }
     }
   }
 
@@ -109,7 +138,7 @@ class CRM_Mjwcividbtools_Form_ClearData extends CRM_Core_Form {
     // auto-rendered in the loop -- such as "qfKey" and "buttons".  These
     // items don't have labels.  We'll identify renderable by filtering on
     // the 'label'.
-    $elementNames = array();
+    $elementNames = [];
     foreach ($this->_elements as $element) {
       /** @var HTML_QuickForm_Element $element */
       $label = $element->getLabel();
