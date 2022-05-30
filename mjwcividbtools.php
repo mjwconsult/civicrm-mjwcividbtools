3346d41ca410937fcd6ac1c9548bbb9a7ef8e573
Matthew Wire <mjw@mjwconsult.co.uk>
2022-05-30 12:19:58 +0100

    Release 0.5

--- mjwcividbtools.php
+++ mjwcividbtools.php
@@ -13,15 +13,6 @@ function mjwcividbtools_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function mjwcividbtools_civicrm_xmlMenu(&$files) {
-  _mjwcividbtools_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
@@ -73,51 +64,6 @@ function mjwcividbtools_civicrm_disable() {
  */
 function mjwcividbtools_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
   return _mjwcividbtools_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
- */
-function mjwcividbtools_civicrm_managed(&$entities) {
-  _mjwcividbtools_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function mjwcividbtools_civicrm_caseTypes(&$caseTypes) {
-  _mjwcividbtools_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_angularModules
- */
-function mjwcividbtools_civicrm_angularModules(&$angularModules) {
-  _mjwcividbtools_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function mjwcividbtools_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _mjwcividbtools_civix_civicrm_alterSettingsFolders($metaDataFolders);
 }
 
 /**
